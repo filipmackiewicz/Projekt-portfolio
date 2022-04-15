427e2e4a93ab3b77725614580b8e59a4ca790d7a
Filip Mackiewicz <mackiewicz.filip@gmail.com>
2022-04-15 13:28:14 +0200

    add smooth scroll to others browsers

---
+++ assets/scripts/smooth-scroll.js
@@ -0,0 +1,24 @@
+function scrollToSmoothly(pos, time) {
+    const currentPos = window.pageYOffset;
+    const start = null;
+    if(time == null) time = 500;
+    pos = +pos, time = +time;
+    window.requestAnimationFrame(function step(currentTime) {
+        start = !start ? currentTime : start;
+        const progress = currentTime - start;
+        if (currentPos < pos) {
+            window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
+        } else {
+            window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
+        }
+        if (progress < time) {
+            window.requestAnimationFrame(step);
+        } else {
+            window.scrollTo(0, pos);
+        }
+    });
+}
+
+document.querySelector('button').addEventListener('click', function(e){
+  scrollToSmoothly(500, 1500);
+});
