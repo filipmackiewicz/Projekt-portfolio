42bddd455ec28d9483e8f5342b7288be6f29cf5f
Filip Mackiewicz <mackiewicz.filip@gmail.com>
2022-04-24 12:16:07 +0200

    - for spaces

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,5 +1,5 @@
 const menu = document.getElementById('menu');
-const links = document.getElementById('menulinks');
+const links = document.getElementById('menu-links');
 const bar1 = document.getElementById('bar1');
 const bar2 = document.getElementById('bar2');
 const bar3 = document.getElementById('bar3');
