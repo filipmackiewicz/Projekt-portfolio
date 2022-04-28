9f360706693fbc3282bcc1533ecbe3b0f2305431
Filip Mackiewicz <mackiewicz.filip@gmail.com>
2022-04-28 13:16:39 +0200

    js changes

--- assets/scripts/smooth-scroll.js
+++ assets/scripts/smooth-scroll.js
@@ -1,24 +1,38 @@
-function scrollToSmoothly(pos, time) {
-    const currentPos = window.pageYOffset;
-    const start = null;
-    if(time == null) time = 500;
-    pos = +pos, time = +time;
-    window.requestAnimationFrame(function step(currentTime) {
-        start = !start ? currentTime : start;
-        const progress = currentTime - start;
-        if (currentPos < pos) {
-            window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
-        } else {
-            window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
-        }
-        if (progress < time) {
-            window.requestAnimationFrame(step);
-        } else {
-            window.scrollTo(0, pos);
-        }
-    });
-}
+class Smoothscroll {
 
-document.querySelector('button').addEventListener('click', function(e){
-  scrollToSmoothly(500, 1500);
-});
+    constructor () {
+        this.UiSelectors = {
+            smooth: '[data-smooth]'
+        }
+    }
+
+    initialize() {
+        this.smooth = document.querySelector(this.UiSelectors.smooth);
+        this.addEventListeners();
+    }
+
+    scrollToSmothly(pos, time) {
+        const currentPos = window.pageYOffset;
+        const start = null;
+        if(time == null) time = 500;
+        pos = +pos, time = +time;
+        window.requestAnimationFrame(function step(currentTime) {
+            start = !start ? currentTime : start;
+            const progress = currentTime - start;
+            if (currentPos < pos) {
+                window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
+            } else {
+                window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
+            }
+            if (progress < time) {
+                window.requestAnimationFrame(step);
+            } else {
+                window.scrollTo(0, pos);
+            }
+        });
+    }
+
+    addEventListeners() {
+        this.smooth.addEventListener('click', () => this.scrollToSmoothly(500, 1500));
+    }
+}
