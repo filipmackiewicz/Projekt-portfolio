71029d5bac73b916886c4dd5cfdb76bea70747ff
Filip Mackiewicz <mackiewicz.filip@gmail.com>
2022-05-10 18:51:42 +0200

    offer section

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -3,7 +3,8 @@ class Menu {
         this.Selectors = {
             bars: '[data-bars]',
             links: '[data-links]',
-            menu: '[data-menu]'
+            menu: '[data-menu]',
+            link: '[data-link]'
         }
 
         this.State = {
@@ -16,16 +17,19 @@ class Menu {
         this.bars = document.querySelector(this.Selectors.bars);
         this.links = document.querySelector(this.Selectors.links);
         this.menu = document.querySelector(this.Selectors.menu);
+        this.link = document.querySelector(this.Selectors.link);
         this.addEventListeners();
     }
 
     showMenu() {
         this.links.classList.toggle(this.State.visible);
         this.bars.classList.toggle(this.State.animate);
+        console.log("zamykam");
     }
 
     addEventListeners() {
         this.menu.addEventListener('click', () => this.showMenu());
+        this.link.addEventListener('click', () => this.showMenu());
     }
 
 }
