af933b6821c2e6ab5d4a9860e4b3312088f7fd1e
Filip Mackiewicz <mackiewicz.filip@gmail.com>
2022-05-10 10:00:53 +0200

    changes

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,22 +1,27 @@
 class Menu {
     constructor () {
-        this.UiSelectors = {
+        this.Selectors = {
             bars: '[data-bars]',
             links: '[data-links]',
             menu: '[data-menu]'
         }
+
+        this.State = {
+            visible: 'is-visible',
+            animate: 'animate'
+        }
     }
 
     initialize() {
-        this.bars = document.querySelector(this.UiSelectors.bars);
-        this.links = document.querySelector(this.UiSelectors.links);
-        this.menu = document.querySelector(this.UiSelectors.menu);
+        this.bars = document.querySelector(this.Selectors.bars);
+        this.links = document.querySelector(this.Selectors.links);
+        this.menu = document.querySelector(this.Selectors.menu);
         this.addEventListeners();
     }
 
     showMenu() {
-        this.links.classList.toggle('is-visible');
-        this.bars.classList.toggle('animate');
+        this.links.classList.toggle(this.State.visible);
+        this.bars.classList.toggle(this.State.animate);
     }
 
     addEventListeners() {
